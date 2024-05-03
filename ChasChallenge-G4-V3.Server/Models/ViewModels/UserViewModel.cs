0553d3b5aa742d1b15ea2c20ae2926e64e55338b
Sean Schelin <119659530+seanie1995@users.noreply.github.com>
2024-05-03 13:15:06 +0200

    Removed "password" from UserViewModel. All passwords are hashed now so it is not possible to view passwords.

--- ChasChallenge-G4-V3.Server/Models/ViewModels/UserViewModel.cs
+++ ChasChallenge-G4-V3.Server/Models/ViewModels/UserViewModel.cs
@@ -3,7 +3,7 @@
     public class UserViewModel
     {
         public string? Name { get; set; }
-        public string? Password { get; set; }
+        
         public string? Email { get; set; }
 
         public List<ChildViewModel>? Children { get; set; }
