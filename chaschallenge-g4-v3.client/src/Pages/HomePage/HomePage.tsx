162c5b2b7ec1af1f2e21503e7ee3cd46b0f02d64
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-06 16:43:26 +0200

    Update homepage styling and add login/signup links

--- chaschallenge-g4-v3.client/src/Pages/HomePage/HomePage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/HomePage/HomePage.tsx
@@ -1,7 +1,20 @@
+import '../../scss/Sass-Pages/_HomePage.scss'
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
     return (
         <>
-        <h1>hithere s</h1>
+        <header className='homepage-header'>
+            <h1 className='homepage-title'>Välkommen till Bebisappen</h1>
+            <h2 className='homepage-subtitle'>Registrera dig och börja er resa här!</h2>
+        </header>
+        <main>
+            <section className='homepage-auth-portal'>
+                <Link className='homepage-login-btn' to='/SignIn'>Logga in</Link>
+                <p>Första gången här?</p>
+                <Link to='/SignUp'>Skapa ett konto här!</Link>
+            </section>
+        </main>
         </>
     )
 }
