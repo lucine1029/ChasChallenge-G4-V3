aa6a82530e248105d04c55a6134d8c77c96cfdc3
LimpanGH <Linusj555@gmail.com>
2024-05-06 14:36:21 +0200

    organized sass-components and nav-bar-components with anders and ruben

--- chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/SingInPage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/SingInPage.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SignInForm from './SignInForm';
-import '../../../scss/Components/_SignUpPage.scss';
+import '../../../scss/Sass-Pages/_SignUpPage.scss';
 
 const SignIn: React.FC = () => {
   return (
-    <div className="login-container"> {/* Använd klassen för container */}
-      <h2 className="login-text">Sign In Page</h2> {/* Använd klassen för text */}
+    <div className='login-container'>
+      {' '}
+      {/* Använd klassen för container */}
+      <h2 className='login-text'>Sign In Page</h2> {/* Använd klassen för text */}
       <SignInForm />
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Link to="/">Back to Home</Link>
+        <Link to='/'>Back to Home</Link>
       </div>
     </div>
   );
-}
+};
 
 export default SignIn;
-
-
-
-
-
-/* export default function SignIn() {
-    return (
-        <>
-        </>
-    )
-} */
