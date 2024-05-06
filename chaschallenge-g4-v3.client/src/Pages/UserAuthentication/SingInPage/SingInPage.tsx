f91cdb4467ca54cdceddd68d2b12f8b95299756a
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-06 17:51:00 +0200

    refactor: Remove unnecessary h1 tag from SignUpPage and update SignInPage UI

--- chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/SingInPage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/SingInPage.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SignInForm from './SignInForm';
 import '../../../scss/Sass-Pages/_SignUpPage.scss';
 
 const SignIn: React.FC = () => {
   return (
     <main>
-    <div className='login-container'>
-      {' '}
-      {/* Använd klassen för container */}
-      <h2 className='login-text'>Sign In Page</h2> {/* Använd klassen för text */}
-      <SignInForm />
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Link to='/'>Back to Home</Link>
+      <div className='login-container'>
+        {' '}
+        {/* Använd klassen för container */}
+        <h2 className='login-text'>Logga In</h2> {/* Använd klassen för text */}
+        <SignInForm />
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        </div>
       </div>
-    </div>
     </main>
   );
 };
