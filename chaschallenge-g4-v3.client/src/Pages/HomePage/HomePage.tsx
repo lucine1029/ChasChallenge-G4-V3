6109636f8daab0a62057997fe117380b7c863b73
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-07 09:02:25 +0200

    refactor: Remove unnecessary h1 tag from SignUpPage and update SignInPage UI

--- chaschallenge-g4-v3.client/src/Pages/HomePage/HomePage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/HomePage/HomePage.tsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 export default function HomePage() {
     return (
         <>
-        <header className='homepage-header'>
-            <h1 className='homepage-title'>Välkommen till Bebisappen</h1>
-            <h2 className='homepage-subtitle'>Registrera dig och börja er resa här!</h2>
-        </header>
-        <main>
-            <section className='homepage-auth-portal'>
-                <Link className='homepage-login-btn' to='/SignIn'>Logga in</Link>
-                <p>Första gången här?</p>
-                <Link to='/SignUp'>Skapa ett konto här!</Link>
-            </section>
-        </main>
+            <header className='homepage-header'>
+                <h1 className='homepage-title'>Välkommen till Bebisappen</h1>
+                <h2 className='homepage-subtitle'>Registrera dig och börja er resa här!</h2>
+            </header>
+            <main>
+                <section className='homepage-auth-portal'>
+                    <Link className='homepage-login-btn' to='/SignIn'>Logga in</Link>
+                    <p>Första gången här?</p>
+                    <Link to='/SignUp'>Skapa ett konto här!</Link>
+                </section>
+            </main>
         </>
     )
 }
