705780f72fff2561c7ea7dab833cf9ae655e2697
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-05-08 17:00:01 +0200

    stylat settingssidan

--- chaschallenge-g4-v3.client/src/Pages/AccountPage/AccountPage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/AccountPage/AccountPage.tsx
@@ -2,6 +2,8 @@
 
 import { Link } from 'react-router-dom';
 import { LiaUserCogSolid } from 'react-icons/lia';
+import { LiaBabySolid } from 'react-icons/lia';
+import { LiaAngleRightSolid } from 'react-icons/lia';
 import '../../scss/Reusable-Components/_list.scss';
 
 const AccountPage = () => {
@@ -11,13 +13,25 @@ const AccountPage = () => {
       <nav>
         <ul>
           <li>
-            <Link to='settings'>
-              <LiaUserCogSolid />
-              Konto
+            <Link className='link-style card' to='settings'>
+              <LiaUserCogSolid className='large-icon' />
+              <div className='push-right'>
+                <h3>Konto</h3>
+                <p>Uppdatera ditt lösenord</p>
+              </div>
+              <LiaAngleRightSolid />
             </Link>
           </li>
           <li>
-            <Link to='children'>Barn</Link>
+            <Link className='link-style card' to='settings'>
+              <LiaBabySolid className='large-icon' />
+
+              <div className='push-right'>
+                <h3>Barn</h3>
+                <p>Uppdatera, lägg till och ta bort barn</p>
+              </div>
+              <LiaAngleRightSolid />
+            </Link>
           </li>
         </ul>
       </nav>
