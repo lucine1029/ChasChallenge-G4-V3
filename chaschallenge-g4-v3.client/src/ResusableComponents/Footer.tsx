e5177ec0f96d352e84e96b2969b62a24cf1fcf0a
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-08 13:22:22 +0200

    chore: Update package dependencies and add new icons to Footer component

--- chaschallenge-g4-v3.client/src/ResusableComponents/Footer.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/Footer.tsx
@@ -1,13 +1,16 @@
 import '../scss/Reusable-Components/_Footer.scss'
+import { IoChatbox, } from "react-icons/io5";
+import { PiChefHat } from "react-icons/pi";
+import { MdFamilyRestroom } from "react-icons/md";
 
 export default function Footer() {
 
     return(
         <footer>
             <div className="footer-btn-container">
-                <a href="" className="footer-btn-recipes"></a>
-                <a href="" className="footer-btn-family"></a>
-                <a href="" className="footer-btn-chat"></a>
+                <a href="" className="footer-btn-recipes"><PiChefHat color='white' size='42'/></a>
+                <a href="" className="footer-btn-family"><MdFamilyRestroom color='white' size='42'/></a>
+                <a href="/chat" className="footer-btn-chat"><IoChatbox color='white' size='42'/></a>
                 <a href="" className="footer-btn-journal"></a>
                 <a href="" className="footer-btn-tba"></a>
             </div>
