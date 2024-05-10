c4220cecc93a9947987bdf90677c8861de713752
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-10 13:00:45 +0200

    feat: Add journal button to footer

--- chaschallenge-g4-v3.client/src/ResusableComponents/Footer.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/Footer.tsx
@@ -2,6 +2,7 @@ import '../scss/Reusable-Components/_Footer.scss'
 import { IoChatbox, } from "react-icons/io5";
 import { PiChefHat } from "react-icons/pi";
 import { MdFamilyRestroom } from "react-icons/md";
+import { IoIosJournal } from "react-icons/io";
 
 export default function Footer() {
 
@@ -11,7 +12,9 @@ export default function Footer() {
                 <a href="" className="footer-btn-recipes"><PiChefHat color='white' size='42'/></a>
                 <a href="" className="footer-btn-family"><MdFamilyRestroom color='white' size='42'/></a>
                 <a href="/chat" className="footer-btn-chat"><IoChatbox color='white' size='42'/></a>
-                <a href="" className="footer-btn-journal"></a>
+                <a href="" className="footer-btn-journal"><IoIosJournal color='white' size='42'/></a>
+
+                {/* Jag vill ha hamburgare!  */}
                 <a href="" className="footer-btn-tba"></a>
             </div>
         </footer>
