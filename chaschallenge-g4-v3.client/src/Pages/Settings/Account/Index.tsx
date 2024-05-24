21a09d6861a27de752b310d17769184700b18529
LimpanGH <Linusj555@gmail.com>
2024-05-24 15:48:53 +0200

    user name now displaying on account-page

--- chaschallenge-g4-v3.client/src/Pages/Settings/Account/Index.tsx
+++ chaschallenge-g4-v3.client/src/Pages/Settings/Account/Index.tsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import HeaderWithBackButton from '../../../ResusableComponents/HeaderWithBackButton';
 import '../../../scss/Sass-Pages/_AccountPage.scss';
+import { getUser } from '../../../ResusableComponents/Requests/userRequest'; // Import the getUser function
 
 const UserSettings = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await getUser();
+        setUser(userData);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -15,6 +29,8 @@ const UserSettings = () => {
     <>
       <HeaderWithBackButton title='Konto' />
       <div className='user-settings-container'>
+        {user && <div className='user-name'>{`${user.firstName} ${user.lastName}`}</div>}
+
         <form className='user-settings-form'>
           <label>
             Email: <input type='email' />
@@ -22,10 +38,7 @@ const UserSettings = () => {
           <label className='password-label'>
             Password:
             <div className='password-input-container'>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                placeholder='Password'
-              />
+              <input type={showPassword ? 'text' : 'password'} placeholder='Password' />
               <FontAwesomeIcon
                 icon={showPassword ? faEye : faEyeSlash}
                 onClick={togglePasswordVisibility}
@@ -36,10 +49,7 @@ const UserSettings = () => {
           <label className='password-label'>
             Confirm password:
             <div className='password-input-container'>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                placeholder='Confirm password'
-              />
+              <input type={showPassword ? 'text' : 'password'} placeholder='Confirm password' />
               <FontAwesomeIcon
                 icon={showPassword ? faEye : faEyeSlash}
                 onClick={togglePasswordVisibility}
