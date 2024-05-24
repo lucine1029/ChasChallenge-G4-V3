1b7ac3519a65a4430dd6ddc28410a2ca7b1ff1f4
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-24 15:49:40 +0200

    chore: Update authRequest.tsx to include registerUser, forgotPassword, and resetPassword functions

--- chaschallenge-g4-v3.client/src/ResusableComponents/Requests/authRequest.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/Requests/authRequest.tsx
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5148';
 
+
+// Register
 interface userData {
     firstname: string,
     lastname: string,
@@ -12,7 +14,7 @@ interface userData {
     password: string
   }
   
-  export const addNewUser = async (userData: userData) => {
+  export const registerUser = async (userData: userData) => {
     try {
       const response = await axios.post(`${BASE_URL}/register`, userData, {
         headers: {
@@ -31,3 +33,27 @@ interface userData {
       }
     }
   };
+
+
+// Forgot Password
+
+  export const forgotPassword = async (email:string) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/forgotPassword`, email,
+            {headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        if (response.status === 200){
+            console.log('Rest link sent to your email', response.data);
+        }
+    } catch (error){
+        if (axios.isAxiosError(error)){
+            console.error('Error response from server:', error.response?.data);
+        } else {
+            console.error('An unexpected error occured:', error);
+        }
+    }
+  }
+
+// Reset Password
