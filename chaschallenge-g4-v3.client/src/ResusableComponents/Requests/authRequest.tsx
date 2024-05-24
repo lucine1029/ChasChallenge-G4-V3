0f7f0dfd3a2cbbed6e48f0c6bbf918ed34983c1a
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-05-24 15:39:03 +0200

    chore: Refactor login and registration functions to use userData object

--- chaschallenge-g4-v3.client/src/ResusableComponents/Requests/authRequest.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/Requests/authRequest.tsx
@@ -1,2 +1,33 @@
 // Rubens komponent
 // Alla från ChasChallenge-G4-V3.Server i swagger
+
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5148';
+
+interface userData {
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string
+  }
+  
+  export const addNewUser = async (userData: userData) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/register`, userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+  
+      if (response.status === 200){
+        console.log('Registration successful:', response.data);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)){
+        console.error('Error response from server:', error.response?.data);
+      } else {
+        console.error('An unexpected error occurred:', error);
+      }
+    }
+  };
