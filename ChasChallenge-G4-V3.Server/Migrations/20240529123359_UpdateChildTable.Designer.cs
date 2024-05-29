c925998f33ccceee39c728f51496b75e2840b874
StinanH <Stina.Hedman@chasacademy.se>
2024-05-29 14:34:31 +0200

    New migrations to update child table

--- ChasChallenge-G4-V3.Server/Migrations/20240529123359_UpdateChildTable.Designer.cs
+++ ChasChallenge-G4-V3.Server/Migrations/20240529123359_UpdateChildTable.Designer.cs
@@ -4,6 +4,7 @@ using ChasChallenge_G4_V3.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChasChallenge_G4_V3.Server.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240529123359_UpdateChildTable")]
+    partial class UpdateChildTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
