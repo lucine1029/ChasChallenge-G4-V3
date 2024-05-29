6b847c8269749b59f2af6c0d4ae22fdf94813ad5
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-05-29 11:34:54 +0200

    just a console.log

--- chaschallenge-g4-v3.client/src/ResusableComponents/Requests/userRequest.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/Requests/userRequest.tsx
@@ -11,13 +11,15 @@ const BASE_URL = 'http://localhost:5148';
 export const getAllUsers = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/allusers`);
-    //console.log(response.data);
+    console.log(response.data);
     return response.data; // Return the data for further processing
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error; // Rethrow the error for handling elsewhere if needed
   }
 };
+
+getAllUsers();
 
 // GET User by ID + Can also be used to get user kids
 export const getUser = async (userId: string) => {
