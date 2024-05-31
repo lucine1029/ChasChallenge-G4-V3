42a25ce1219df2676a8981591892172294c56ff0
LimpanGH <Linusj555@gmail.com>
2024-05-31 16:32:45 +0200

    swapped to gtp4o, added urls-links in the ai-reply, styled the child-modal

--- chaschallenge-g4-v3.client/src/ResusableComponents/ChildModal.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/ChildModal.tsx
@@ -6,7 +6,8 @@ interface ChildData {
   gender: string;
   birthdate: string;
   allergies: { name: string }[];
-  imageSource: string
+  imageSource: string;
+  measurements: string;
 }
 
 interface Props {
@@ -25,13 +26,12 @@ const ChildAccordion: React.FC<Props> = ({ childData, closeModal }) => {
           <div className='card-body'>
             <h5 className='card-title'>{childData.name}</h5>
             <img src={childData.imageSource} alt={childData.name} /> {/* Display the image */}
-
-            <p className='card-text'>bild: {childData.imageSource}</p>
+            {/* <p className='card-text'>bild: {childData.imageSource}</p> */}
             <p className='card-text'>Smeknamn: {childData.nickName}</p>
             <p className='card-text'>Kön: {childData.gender}</p>
             <p className='card-text'>Född: {new Date(childData.birthdate).toLocaleDateString()}</p>
             <p className='card-text allergies'>
-                
+            <p className='card-text' > Längd: 78 cm {childData.measurements}</p>
               Allergier:{' '}
               {childData.allergies.map((allergy, allergyIndex) => (
                 <span key={allergyIndex}>
