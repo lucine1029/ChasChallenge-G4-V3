e270adc3842ba4822d09c5d0fe466735052b8450
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-06-03 20:38:40 +0200

    Update SignUpPage, ForgotPasswordPage, and SignInPage components to wrap the forms in a section with the class 'section-container' for consistent styling.

--- chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/ForgotPasswordPage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/ForgotPasswordPage.tsx
@@ -4,20 +4,22 @@ export default function ForgotPasswordPage() {
 
     return(
         <>
-        <header className='header'>
+        <header className='header-container'>
             {/* <img src={} alt='Babster Logo' className='logo' /> */}
             <span className='logo'>Babster</span>
             <button className='header-button'>Logga in</button>
         </header>
         <main>
-            <form action="">
-                <div className="input-container">
-                    <h2>Återställ ditt lösenord</h2>
-                    <p>Vi skickar en länk till din epost-adress för att återställa ditt lösenord </p>
-                    <input type="email" className='input-field' placeholder="E-mail"/>
-                    <button className="forgot-submit-btn">Skicka</button>
-                </div>
-            </form>
+            <section className='section-container'>
+                <form action="">
+                    <div className="input-container">
+                        <h2>Återställ ditt lösenord</h2>
+                        <p>Vi skickar en länk till din epost-adress för att återställa ditt lösenord </p>
+                        <input type="email" className='input-field' placeholder="E-mail"/>
+                        <button className="forgot-submit-btn">Skicka</button>
+                    </div>
+                </form>
+            </section>
         </main>
     </>
     )
