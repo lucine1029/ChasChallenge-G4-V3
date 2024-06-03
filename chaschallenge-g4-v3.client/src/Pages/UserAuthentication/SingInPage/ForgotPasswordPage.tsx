707b1e043b70cb6831f293885ad6c7ed063db811
Ruben Ahlin <Ruben.Ahlin@Chasacademy.se>
2024-06-03 15:17:29 +0200

    Update ForgotPasswordPage component styling and form structure

--- chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/ForgotPasswordPage.tsx
+++ chaschallenge-g4-v3.client/src/Pages/UserAuthentication/SingInPage/ForgotPasswordPage.tsx
@@ -1,11 +1,18 @@
+import '../../../scss/Sass-Pages/_ForgotPassword.scss'
+
 export default function ForgotPasswordPage() {
 
     return(
-    <>
+        <>
         <main>
-            <div>
-                <input type="email" name="" id="" placeholder="E-mail"/>
-            </div>
+            <form action="">
+                <div className="input-container">
+                    <h2>Återställ ditt lösenord</h2>
+                    <p>Vi skickar en länk till din epost-adress för att återställa ditt lösenord </p>
+                    <input type="email" className='input-field' placeholder="E-mail"/>
+                    <button className="forgot-submit-btn">Skicka</button>
+                </div>
+            </form>
         </main>
     </>
     )
