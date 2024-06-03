815459c71d7730252ec1ed9e1e3cc17acdc2f812
LimpanGH <Linusj555@gmail.com>
2024-06-03 12:39:33 +0200

    added styling to chatpage

--- chaschallenge-g4-v3.client/src/ResusableComponents/ChildModal.tsx
+++ chaschallenge-g4-v3.client/src/ResusableComponents/ChildModal.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LiaUserEditSolid } from 'react-icons/lia';
 
 interface ChildData {
   name: string;
@@ -22,16 +24,24 @@ const ChildAccordion: React.FC<Props> = ({ childData, closeModal }) => {
         <span className='close' onClick={closeModal}>
           &times;
         </span>
+
+        <span className='link-to-settings' onClick={handleKebabClick}>
+          <LiaUserEditSolid />
+        </span>
+
         <div className='card'>
           <div className='card-body'>
             <h5 className='card-title'>{childData.name}</h5>
-            <img src={childData.imageSource} alt={childData.name} /> {/* Display the image */}
+            <img src={childData.imageSource} alt={childData.name} />{' '}
+            {/* Display the image */}
             {/* <p className='card-text'>bild: {childData.imageSource}</p> */}
             <p className='card-text'>Smeknamn: {childData.nickName}</p>
             <p className='card-text'>Kön: {childData.gender}</p>
-            <p className='card-text'>Född: {new Date(childData.birthdate).toLocaleDateString()}</p>
+            <p className='card-text'>
+              Född: {new Date(childData.birthdate).toLocaleDateString()}
+            </p>
             <p className='card-text allergies'>
-            <p className='card-text' > Längd: 78 cm {childData.measurements}</p>
+              <p className='card-text'>Längd: {childData.measurements}</p>
               Allergier:{' '}
               {childData.allergies.map((allergy, allergyIndex) => (
                 <span key={allergyIndex}>
